7f9b3430e839a697dc57176b6f22a2d061e759af
Stepan <steppp1518@gmail.com>
2023-04-08 17:05:05 +0300

    Use POINFLOAT and optimize program

--- graph.c
+++ graph.c
@@ -1,30 +1,47 @@
+#include <windows.h>
 #include <gl/gl.h>
 #define LENGTH 1000000
 
 static void drawOS(const float min_x, const float max_x, const float min_y, const float max_y);
-static void draw(float* val_func, const float min_y, const float max_y, const float min_x, const float max_x, const float step);
+static void draw(POINTFLOAT* points);
 
-float val_func[LENGTH];
+POINTFLOAT points[LENGTH];
+int isCalc = 1;
+float min_y, max_y;
 
 void graph(float (*f)(const float), const float min_x, const float max_x)
 {
     const float step = (max_x - min_x) / LENGTH;
 
-    val_func[0] = f(min_x);
-    float min_y = val_func[0], max_y = val_func[0];
-    float x = min_x + step;
-
-    for (int i = 1; i < LENGTH; i++, x += step)
+    if (isCalc)
     {
-        val_func[i] = f(x);
-        if (min_y > val_func[i])
-            min_y = val_func[i];
-        if (max_y < val_func[i])
-            max_y = val_func[i];
+        points[0].y = f(min_x);
+        min_y = points[0].y, max_y = points[0].y;
+        float x = min_x + step;
+
+        for (int i = 1; i < LENGTH; i++, x += step)
+        {
+            points[i].y = f(x);
+            if (min_y > points[i].y)
+                min_y = points[i].y;
+            if (max_y < points[i].y)
+                max_y = points[i].y;
+        }
+
+        const float scale_max = 1.0f, scale_min = -1.0f;
+        x = min_x;
+        for (int i = 0; i < LENGTH; i++, x += step)
+        {
+            const float proc_x = (x - min_x) / (max_x - min_x), proc_y = (points[i].y - min_y) / (max_y - min_y);
+            points[i].x = proc_x * (scale_max - scale_min) + scale_min;
+            points[i].y = proc_y * (scale_max - scale_min) + scale_min;
+        }
+
+        isCalc = 0;
     }
 
     glLineWidth(2);
-    draw(val_func, min_y, max_y, min_x, max_x, step);
+    draw(points);
     drawOS(min_x, max_x, min_y, max_y);
 }
 
@@ -66,16 +83,11 @@ static void drawOS(const float min_x, const float max_x, const float min_y, cons
     glEnd();
 }
 
-static void draw(float* val_func, const float min_y, const float max_y, const float min_x, const float max_x, const float step)
+static void draw(POINTFLOAT* points)
 {
-    const float scale_max = 1.0f, scale_min = -1.0f;
     glBegin(GL_LINE_STRIP);
     glColor3f(0.0, 1.0, 0.0);
-    float x = min_x;
-    for (int i = 0; i < LENGTH; i++, x += step)
-    {
-        const float proc_x = (x - min_x) / (max_x - min_x), proc_y = (val_func[i] - min_y) / (max_y - min_y);
-        glVertex2f(proc_x * (scale_max - scale_min) + scale_min, proc_y * (scale_max - scale_min) + scale_min);
-    }
+    for (int i = 0; i < LENGTH; i++)
+        glVertex2f(points[i].x, points[i].y);
     glEnd();
 }
